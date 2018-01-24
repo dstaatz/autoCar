fc0902a943cee659161da549a48480967e00456e
Dylan Staatz <dms9800@gmail.com>
2018-01-23 21:50:25 -0500

    Tested car.py with car-py-test.py

--- car-py-test.py
+++ car-py-test.py
@@ -1,12 +1,19 @@
 
 from car import *
+import time
 
-car = Car(reverse_d=True, reverse_s=True)
+car = Car()
 
 car.forward_left()
+time.sleep(1)
 car.forward()
+time.sleep(1)
 car.forward_right()
+time.sleep(1)
 car.back_left()
+time.sleep(1)
 car.back()
+time.sleep(1)
 car.back_right()
+time.sleep(1)
 car.stop()
