1504233e2abcae52b5a1bc4b83e0405b0ff2d533
Dylan Staatz <dms9800@gmail.com>
2018-01-23 20:28:38 -0600

    Added car-py-test.py

---
+++ car-py-test.py
@@ -0,0 +1,12 @@
+
+from car import *
+
+car = Car(reverse_d=True, reverse_s=True)
+
+car.forward_left()
+car.forward()
+car.forward_right()
+car.back_left()
+car.back()
+car.back_right()
+car.stop()
